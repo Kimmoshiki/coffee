0952b82b0473f0e004e50640a53f2f166c5df97f
kimhyunji <112531696+Kimmoshiki@users.noreply.github.com>
2022-10-07 16:02:35 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,5 +1,12 @@
 // p.292
 const badgeEl = document.querySelector('.badges');
+const topBtnEl = document.querySelector('.top_btn');
+
+topBtnEl.addEventListener('click',function(){
+    gsap.to(window,.6,{//브라우저를 선택하여 0.6초만에
+        scrollTo:0 //스크롤 꼭때기 0까지 올라간다.
+    });
+})
 
 window.addEventListener('scroll',function(){
     console.log(window.scrollY);//스크롤값 확인하기
@@ -8,13 +15,22 @@ window.addEventListener('scroll',function(){
         gsap.to(badgeEl, 0.6, {
             opacity:0,
             display:'none'
-        })
+        });
+        gsap.to(topBtnEl, 0.6, {
+            opacity:1,
+            
+        });
     }else{
         // badgeEl.style.display='block';
         gsap.to(badgeEl, 0.6, {
             opacity:1,
             display:'block'
-        })
+        });
+        gsap.to(topBtnEl, 0.6, {
+            opacity:0,
+            
+        });
+        
     }
 });
 
@@ -42,3 +58,18 @@ toggleBtnEl.addEventListener('click',function(){
         promotionEl.classList.add('hide')//높이가 663이 되어야 add-추가
     }
 })
+
+//6, 7, 8, 10콘텐츠가 화면의 80%지점에 보일때 동작하는 스크립트
+const spyEl = document.querySelectorAll('section.scroll-spy');
+spyEl.forEach(function(spyEl){
+    new ScrollMagic
+    .Scene({//감시할 장면을 추가
+      triggerElement:spyEl,
+      triggerHook:0.8 //화면의 80% 지점에서 동작
+    })
+    .setClassToggle(spyEl, 'show')//show클래스를 적용하거나 해체하기
+    .addTo(new ScrollMagic.Controller());//컨트롤러에 장면에 할당한다.
+})
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
